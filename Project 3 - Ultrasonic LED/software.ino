68d79fe824dec0792c31c0502cfec830351bf27c
Zachary <zprager1@yahoo.com>
2025-05-20 21:42:44 -0400

    Update software.ino

--- Project 3 - Ultrasonic LED/software.ino
+++ Project 3 - Ultrasonic LED/software.ino
@@ -1,1 +1,30 @@
+#define TRIG 5 //Green
+#define ECHO 6  //Orange
+#define LED 3 //Yellow
 
+float distance, duration;
+
+void setup() {
+  pinMode(TRIG, OUTPUT); 
+  pinMode(ECHO, INPUT);
+  pinMode(LED, OUTPUT);
+  
+}
+void loop() {
+  
+  digitalWrite(TRIG, LOW);  
+  delayMicroseconds(2);  
+  digitalWrite(TRIG, HIGH);  
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);    
+ 
+  duration = pulseIn(ECHO, HIGH);
+  distance = (duration * .0343) / 2;   
+  if (distance >= 25) {
+    digitalWrite(LED, HIGH);
+    }
+  else {
+    digitalWrite(LED, LOW);
+  }
+  delay(100);
+}
