7d63021bc4675c9081059eb68f9e40f72e701c41
Zachary <zprager1@yahoo.com>
2025-05-22 21:17:41 -0400

    Update software.ino

--- Project 5 - LCD Message/software.ino
+++ Project 5 - LCD Message/software.ino
@@ -2,7 +2,7 @@
 
 const int rs = 12, e = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
 
 void setup() {
   lcd.begin(16, 2);
